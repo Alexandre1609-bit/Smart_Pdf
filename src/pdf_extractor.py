d80490c6ebbcce55ca4e7169e848d9ba955419ac
alexandre regnier <alexandre.regnier59@live.fr>
2025-09-05 11:50:27 +0200

    Add pdf_extractor script, WIP

---
+++ src/pdf_extractor.py
@@ -0,0 +1,36 @@
+import pymupdf 
+
+
+def get_book_content():
+    while True:
+        try:
+            pdf_path = input("Select file: ")
+            doc = pymupdf.open(pdf_path)
+            break
+        except:
+            print("File not found")
+            continue
+    num_pages = doc.page_count
+    book_content = []
+    for i in range(0, num_pages):
+        page = doc.load_page(i)
+        page_text = page.get_text("text")
+        raw_txt = page_text.replace("\t","")
+        book_content.append(raw_txt)
+        book_content.append(f"\n\n --- page {i+1} break --- \n\n")
+    return book_content
+
+
+def txt_write(book_content):
+
+    with  open(input("Choose file's name: ") + ".txt", "w", encoding="utf-8") as f:
+        f.write("".join(book_content))
+    
+data = get_book_content()
+txt_write(data)
+
+
+
+
+
+
